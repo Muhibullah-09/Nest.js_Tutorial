477a208805f1ed0f69cb1c23277ff2cd514c578a
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-08-06 01:14:39 +0500

    controllers part done

--- src/students/student.controller.ts
+++ src/students/student.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put } from '@nestjs/common';
 
 @Controller('students')
 export class StudentController {
+
   //get all the students
   @Get()
   getStudents() {
@@ -10,8 +11,20 @@ export class StudentController {
 
   //get a specific student
   //: ==> colon represents that its a dynamic route.
-  @Get('/:id')
+  @Get('/:studentId')
   getStudentById() {
     return 'Get Student by Id';
+  }
+
+  //create a new student
+  @Post('/createStudent')
+  createStudent() {
+    return 'Create Student';
+  }
+
+  //update a student
+  @Put('/updateStudent/:studentId')
+  updateStudent() {
+    return 'Update Student by Id';
   }
 }
--- src/teachers/teacher.controller.ts
+++ src/teachers/teacher.controller.ts
@@ -1,10 +1,24 @@
-import {Controller, Get} from '@nestjs/common';
-
+import { Controller, Get, Put } from '@nestjs/common';
 
 @Controller('teachers')
 export class TeacherController {
-    @Get()
-    getAllTeachers() {
-        return "All data of teachers"
-    }
+  @Get()
+  getTeachers() {
+    return 'All data of teachers';
+  }
+
+  @Get('/:teacherId')
+  getTeacherById() {
+    return 'Teacher by ID.';
+  }
+
+  @Get('/:teacherId/students')
+  getStudentsByTeacherId() {
+    return 'Get students by teacher ID';
+  }
+
+  @Put('/:teacherId/students/:studentId')
+  updateStudentByTeacherId() {
+    return 'Update student by teacher ID';
+  }
 }
