69f07e8e200ff375e0da5a83f9be24828f895e6f
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-08-09 19:08:12 +0500

    add modules middleware pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { StudentService } from '../student/student.service';
-import { TeacherController } from '../teacher/teacher.controller';
-import { StudentController } from '../student/student.controller';
-import { TeacherService } from 'src/teacher/teacher.service';
-import { StudentTeacherController } from 'src/teacher/student.controller';
+import { StudentModule } from 'src/student/student.module';
+import { TeacherModule } from 'src/teacher/teacher.module';
 
 @Module({
-  imports: [],
-  controllers: [StudentController , TeacherController, StudentTeacherController],
-  providers: [StudentService , TeacherService],
+  imports: [TeacherModule ,StudentModule],
+  // controllers: [StudentController , TeacherController, StudentTeacherController],
+  // providers: [StudentService , TeacherService],
 })
 export class AppModule {}
